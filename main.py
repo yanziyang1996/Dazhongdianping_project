b12ff6eb5099fd1d0fd1d97c4ef736f3c7be864e
yanziyang1996 <598125055@qq.com>
2017-08-02 18:29:30 +0800

    add code

---
+++ main.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+# coding date：2017/8/1 by Jerry.Yan,E-mail:598125055@qq.com " all right reserved"
+from scrapy import cmdline
+
+
+cmdline.execute("scrapy crawl dazhongdianping_shop".split())
